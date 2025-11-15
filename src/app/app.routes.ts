58900ddbe7675359f11eb1f7df6f052071bcc3d9
sergio sanhueza <sergio@naitus.cl>
2025-11-15 12:28:03 -0300

    feat: add initial routes to use the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './modules/layout/presentation/layout.component';
+import { HomeComponent } from './modules/home/presentation/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                loadComponent() {
+                    return import('./modules/home/presentation/home.component').then(m => m.HomeComponent);
+                }
+            },
+            {
+                path: 'contacto',
+                loadComponent() {
+                    return import('./modules/contact/presentation/contact.component').then(m => m.ContactComponent);
+                }
+            },
+            {
+                path: ':category',
+                loadComponent() {
+                    return import('./modules/categories/presentation/categories.component').then(m => m.CategoriesComponent);
+                }
+            }
+        ],
+    }
+];
